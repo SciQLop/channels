91f38bce887b53f165201fb27fa5988fae527c5b
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-06-13 22:48:28 +0200

    Basic implementation tested in single thread.
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

---
+++ channels/channels.hpp
@@ -0,0 +1,118 @@
+/*------------------------------------------------------------------------------
+-- This file is a part of the SciQLop Software
+-- Copyright (C) 2020, Plasma Physics Laboratory - CNRS
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+-------------------------------------------------------------------------------*/
+/*-- Author : Alexis Jeandet
+-- Mail : alexis.jeandet@member.fsf.org
+----------------------------------------------------------------------------*/
+#pragma once
+#include <condition_variable>
+#include <queue>
+#include <type_traits>
+
+namespace channels
+{
+namespace full_policy {
+struct wait_for_space
+{
+};
+struct overwrite_last
+{
+};
+}
+namespace details
+{
+
+template<typename ref_type, typename... types>
+inline constexpr bool is_any_of_v = (std::is_same_v<ref_type,types>||...);
+
+    template <typename T, std::size_t max_size, typename full_policy_t>
+    struct _fixed_size_queue
+    {
+        static_assert (is_any_of_v<full_policy_t,
+            full_policy::overwrite_last,full_policy::wait_for_space>,
+        "Unknown policy");
+
+        inline void add(T&& item)
+        {
+            using namespace full_policy;
+            std::unique_lock<std::mutex> lock(m_mutex);
+            if constexpr (std::is_same_v<full_policy_t, wait_for_space>)
+            {
+                m_cv.wait(lock, [this]() { return !full(); });
+            }
+            else if constexpr (std::is_same_v<full_policy_t, overwrite_last>)
+            {
+                if (full())
+                    m_queue.pop_back();
+            }
+            m_queue.push_back(std::forward<T>(item));
+        }
+
+        inline T take()
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_cv.wait(lock, [this]() { return !empty(); });
+            T item = m_queue.front();
+            m_queue.pop_front();
+            return item;
+        }
+
+        inline bool full() const noexcept { return size() >= (max_size); }
+        inline bool empty() const noexcept { return size() == 0U; }
+        inline std::size_t size() const noexcept { return std::size(m_queue); }
+
+    private:
+        std::deque<T> m_queue;
+        std::mutex m_mutex;
+        std::condition_variable m_cv;
+    };
+
+
+}
+
+template <typename T, std::size_t max_size, typename full_policy_t = full_policy::wait_for_space>
+struct channel
+{
+    using value_type = T;
+
+    channel()=default;
+    channel(const channel&)=delete;
+    channel& operator=(const channel&)=delete;
+
+    inline channel& operator<<(T&& item)
+    {
+        add(std::forward<T>(item));
+        return *this;
+    }
+
+    inline channel& operator<<(T& item)
+    {
+        item = take();
+        return *this;
+    }
+
+    inline T take() { return m_queue.take(); }
+    inline void add(T&& item) { m_queue.add(std::forward<T>(item)); }
+
+    inline std::size_t size() const noexcept { return std::size(m_queue); }
+
+private:
+    details::_fixed_size_queue<T, max_size, full_policy_t> m_queue;
+};
+
+}
---
+++ tests/test1/main.cpp
@@ -0,0 +1,92 @@
+/*------------------------------------------------------------------------------
+-- This file is a part of the SciQLop Software
+-- Copyright (C) 2020, Plasma Physics Laboratory - CNRS
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+-------------------------------------------------------------------------------*/
+/*-- Author : Alexis Jeandet
+-- Mail : alexis.jeandet@member.fsf.org
+----------------------------------------------------------------------------*/
+#include <channels/channels.hpp>
+
+#define CATCH_CONFIG_MAIN
+#if __has_include(<catch2/catch.hpp>)
+#include <catch2/catch.hpp>
+#else
+#include <catch.hpp>
+#endif
+
+SCENARIO("Single thread", "[Channels]")
+{
+    GIVEN("a single slot channel")
+    {
+        channels::channel<int, 1, channels::full_policy::overwrite_last> chan;
+        WHEN("Nothing is inside")
+        {
+            THEN("Size should be 0") { REQUIRE(std::size(chan) == 0); }
+        }
+        WHEN("adding one item")
+        {
+            chan << -1;
+            THEN("Size should be 1 and item can be retreived")
+            {
+                REQUIRE(std::size(chan) == 1);
+                REQUIRE(chan.take() == -1);
+                REQUIRE(std::size(chan) == 0);
+            }
+        }
+        WHEN("adding two items")
+        {
+            chan << -1 << -2;
+            THEN("Size should be still 1 and last item can be retreived")
+            {
+                REQUIRE(std::size(chan) == 1);
+                REQUIRE(chan.take() == -2);
+                REQUIRE(std::size(chan) == 0);
+            }
+        }
+    }
+
+    GIVEN("a buffered channel")
+    {
+        channels::channel<int, 5, channels::full_policy::overwrite_last> chan;
+        WHEN("Nothing is inside")
+        {
+            THEN("Size should be 0") { REQUIRE(std::size(chan) == 0); }
+        }
+        WHEN("adding one item")
+        {
+            chan << -1;
+            THEN("Size should be 1 and item can be retreived")
+            {
+                REQUIRE(std::size(chan) == 1);
+                REQUIRE(chan.take() == -1);
+                REQUIRE(std::size(chan) == 0);
+            }
+        }
+        WHEN("adding two items")
+        {
+            chan << -10 << -2;
+            THEN("Size should be 2 and both two items can be retreived")
+            {
+                REQUIRE(std::size(chan) == 2);
+                REQUIRE(chan.take() == -10);
+                REQUIRE(std::size(chan) == 1);
+                REQUIRE(chan.take() == -2);
+                REQUIRE(std::size(chan) == 0);
+            }
+        }
+    }
+}
