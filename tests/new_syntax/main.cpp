71b9ca7ced8e488d4d26e85300b485a71670d0aa
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2021-01-15 10:54:15 +0100

    Pipeline construction with new syntax wroks
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/new_syntax/main.cpp
+++ tests/new_syntax/main.cpp
@@ -20,6 +20,7 @@
 -- Mail : alexis.jeandet@member.fsf.org
 ----------------------------------------------------------------------------*/
 #include <channels/channels.hpp>
+#include <channels/pipelines.hpp>
 #include <functional>
 #include <thread>
 
@@ -39,22 +40,33 @@ T coef(T value)
 template <int b, typename T>
 T offset(T value)
 {
-    return value - b;
+    return value + b;
 }
 
 SCENARIO("new_syntax", "[Channels]")
 {
     using namespace std::placeholders;
+    using namespace channels::operators;
+
     using chan_t = channels::channel<int, 128, channels::full_policy::wait_for_space>;
 
-    auto pipeline = chan_t {} >> [](int value) { return value * 10; };
+
+    auto pipeline = chan_t {} >> coef<10, int>;
 
     auto pipeline2
         = chan_t {} >> [](int value) { return value * 10; } >> [](int value) { return value - 1; };
 
-    auto pipeline3 = chan_t {} >> coef<10, int> >> offset<1, int>;
+    auto pipeline3 = chan_t {} >> coef<10, int> >> offset<-1, int>;
 
     auto pipeline4 = chan_t {} >> std::bind(std::multiplies<int> {}, 10, _1)
         >> std::bind(std::plus<int> {}, -1, _1);
 
+    pipeline.add(10);
+    REQUIRE(*pipeline.take() == 100);
+    pipeline2 << 10;
+    REQUIRE(*pipeline2.take() == 99);
+    pipeline3 << 10;
+    REQUIRE(*pipeline3.take() == 99);
+    pipeline4 << 10;
+    REQUIRE(*pipeline4.take() == 99);
 }
